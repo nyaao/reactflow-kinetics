801ca651349ff914c98199333a298ee76aab9ea2
oshima@jgc <n.oshima216@gmail.com>
2023-03-15 08:44:41 +0900

    AWS lambdaからDjango RestFrameworkに移行 Changes to be committed: 	modified:   code/src/content/submit.ts
    Changes not staged for commit:
    	modified:   django/API/API/settings.py
    	modified:   django/API/App/apis.py
    	deleted:    django/API/App/myNetworkX.py

--- code/src/content/submit.ts
+++ code/src/content/submit.ts
@@ -3,9 +3,8 @@ import { createDefaultNodeParams } from './default';
 import { Node, Edge } from 'reactflow';
 
 export const calc=async(nodes:Node[],edges:Edge[])=>{
-    const res = await axios.post('https://qpl98gtxdi.execute-api.ap-northeast-1.amazonaws.com/dev/api/kinetics_equation/',{body:{nodes:nodes,edges:edges}})
-    // axios.post('http://127.0.0.1:8000/apitest/',{Nodes:nodes,Edges:edges})
-
+    // const res = await axios.post('https://*****.****.****/',{body:{nodes:nodes,edges:edges}})
+    const res = await axios.post('http://127.0.0.1:8000/apitest/',{body:{nodes:nodes,edges:edges}})
     const retnodes:Node[] = JSON.parse(res.data.nodes);
     const newnodes = nodes.map((n,i)=>(Object.assign({},
       createDefaultNodeParams(n.type as string, n.position),
