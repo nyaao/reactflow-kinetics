b31759a7dd8a388770c8144dcf65a1ab93b473f0
oshima <n.oshima216@gmail.com>
2023-03-18 13:40:37 +0900

    id不必要に増えてしまう不具合の修正
    Changes to be committed:
    	modified:   code/src/content/submit.ts

--- code/src/content/submit.ts
+++ code/src/content/submit.ts
@@ -7,8 +7,10 @@ export const calc=async(nodes:Node[],edges:Edge[])=>{
     const res = await axios.post('http://127.0.0.1:8000/apitest/',{body:{nodes:nodes,edges:edges}})
     const retnodes:Node[] = JSON.parse(res.data.nodes);
     const newnodes = nodes.map((n,i)=>(Object.assign({},
-      createDefaultNodeParams(n.type as string, n.position),
-      {id:n.id},
+      {id:n.id,
+      type:n.type,
+      position:n.position,
+      zIndex:100},
       {data:Object.assign({},n.data,retnodes.filter(rn=>rn.id===n.id)[0]["data"])}
     )));
     return {newnodes:newnodes}
